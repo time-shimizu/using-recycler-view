da905cd50da94a4fe1a6eb4a4df0d1cba02c1ae3
time-shimizu <213.f.r48132@gmail.com>
2022-03-13 16:15:49 +0900

    implement gridlayout

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.sleeptracker.SleepNightAdapter
 import com.example.android.trackmysleepquality.database.SleepDatabase
@@ -117,6 +118,11 @@ class SleepTrackerFragment : Fragment() {
                 sleepTrackerViewModel.doneNavigating()
             }
         })
+
+        val manager = GridLayoutManager(activity, 3, GridLayoutManager.VERTICAL, false)
+
+        binding.sleepList.layoutManager = manager
+
         return binding.root
     }
 }
